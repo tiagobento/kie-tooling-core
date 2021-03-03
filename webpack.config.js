9dd5f0b590154c1e68e129bfcf7314426f1aff85
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2021-03-03 12:47:07 -0300

    Update Monaco to be smaller and not include TS Worker. (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,30 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/standalone.js',
-  output: {
-    filename: 'monaco.min.js',
-    path: path.resolve(__dirname, 'dist/standalone')
-  },
-  resolveLoader: {
-    alias: {
-      'blob-url-loader': require.resolve('./loaders/blobUrl'),
-      'compile-loader': require.resolve('./loaders/compile')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(svg|ttf|eot|woff|woff2)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /codicon.css$/,
-        use: [
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              config: {
-                path: './config/'
-              }
+    mode: 'production',
+    entry: {
+        standalone: './src/standalone.js'
+    },
+    output: {
+        filename: 'monaco.min.js',
+        path: path.resolve(__dirname, 'dist/standalone')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.ttf$/,
+                use: ["null-loader"],
             }
-          }
         ]
-      }
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        })
     ]
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+};
